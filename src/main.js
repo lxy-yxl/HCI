0ec5e6dff2c67be0f98200f30e16ee4a62c197fc
lxy-yxl <chengjing4373@outlook.com>
2022-06-05 16:40:41 +0800

    修复版本问题

--- src/main.js
+++ src/main.js
@@ -3,12 +3,26 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueI18n  from 'vue-i18n' //导入包
+
+Vue.use(VueI18n);      //使用use挂载
+const i18n=new VueI18n({    //通过new出一个i18n的实例
+  locale: 'zh-CN',    // 语言标识 //this.$i18n.locale // 通过切换locale的值来实现语言切换
+  messages: {
+    'zh-CN': require('./common/lang/zh'),   // 中文语言包
+    'en-US': require('./common/lang/en'),   // 英文语言包
+  }
+})
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  i18n,
   router,
   components: { App },
   template: '<App/>'
